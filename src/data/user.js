beedd3c705db22a9d607738e52ba238b8c1fc687
Mr. Vishnu Rathod <vishnurathod5884@gmail.com>
2024-07-15 12:35:29 +0530

    updated project

--- src/data/user.js
+++ src/data/user.js
@@ -24,7 +24,7 @@ const INFO = {
 		
 
 
-		title: "I am a Database engineer developer based in data science I am an Data science undergraduate from GHRIBM.",
+		title: "I am a Database engineer developer based in data science I am an Data science undergraduate from GHRIBM.jalgaon",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
